7ac4ec482a61312c32fbffacbf2fbd7be7e6ae53
charlyrm14 <charlyrm14@gmail.com>
2025-05-23 20:10:47 -0600

    feature; vistas admin y menu usuario header

---
+++ src/components/admin/layout/AdminFooter.vue
@@ -0,0 +1,51 @@
+<script setup>
+    import { RouterLink, useRoute } from 'vue-router';
+
+    const route = useRoute()
+</script>
+
+<template>
+<footer class="fixed bottom-0 left-0 right-0 bg-white border border-gray-300 shadow-lg dark:border-0 dark:bg-gray-900 p-3 flex justify-around items-center z-20 mx-3 mb-3 rounded-lg md:hidden">
+        <RouterLink 
+            :to="{ name: 'chat-ia' }"
+            class="flex flex-col items-center text-xs cursor-pointer"
+            :class="route.name === 'admin-home' ? 'text-blue-700' : 'text-gray-500 dark:text-gray-500' ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                </svg>
+        </RouterLink>
+        <RouterLink
+            to="#"
+            class="flex flex-col items-center text-xs cursor-pointer"
+            :class="route.name === 'mi-progreso' ? 'text-blue-700' : 'text-gray-500 dark:text-gray-500' ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
+                </svg> 
+        </RouterLink>
+        <RouterLink
+            to="#"
+            class="flex flex-col items-center text-xs cursor-pointer"
+            :class="route.name === 'mis-clases' ? 'text-blue-700' : 'text-gray-500 dark:text-gray-500' ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                </svg>
+        </RouterLink>
+        <RouterLink 
+            :to="{ name: 'eventos' }"
+            class="flex flex-col items-center text-blue-500 text-xs cursor-pointer"
+            :class="route.name === 'eventos' ? 'text-blue-700' : 'text-gray-500 dark:text-gray-500' ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" />
+                </svg>
+        </RouterLink>
+        <RouterLink 
+            :to="{ name: 'home' }"
+            class="flex flex-col items-center text-xs cursor-pointer"
+            :class="route.name === 'home' ? 'text-blue-700' : 'text-gray-500 dark:text-gray-500' ">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+        </RouterLink>
+    </footer>
+</template>
--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,10 @@
 <script setup>
     import { useUserStore } from '@/stores/user';
+import { ref } from 'vue';
 
     const user = useUserStore()
+    const dropdownMenu = ref(false)
+
 </script>
 
 <template>
@@ -11,13 +14,90 @@
                 <p class="text-lg uppercase dark:text-slate-100">King Dreams</p>
             </div>
             <div class="flex items-center justify-between gap-3">
-                <button
-                    @click="user.logOut "
-                    class="uppercase font-light inline-flex items-center gap-1 text-red-500 dark:text-red-600 cursor-pointer hover:opacity-75">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
-                        </svg> Cerrar sesión
-                </button>
+                <div class="relative">
+                    <div class="relative">
+                        <img
+                            @click="dropdownMenu = !dropdownMenu"
+                            src="../../assets/img/300-27.jpg" 
+                            alt="Profile image"
+                            class="rounded-full w-13 h-13 p-1 border-2 border-cyan-500 cursor-pointer">
+                        <div
+                            @click="dropdownMenu = !dropdownMenu"
+                            class="absolute -bottom-1 right-2 bg-gray-500 dark:bg-slate-500 rounded-full w-6 h-6 cursor-pointer hover:opacity-75">
+                                <svg
+                                    v-if="!dropdownMenu"
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    fill="none" 
+                                    viewBox="0 0 24 24" 
+                                    stroke-width="1.5" 
+                                    stroke="currentColor" 
+                                    class="size-6 text-white">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                                </svg>
+                                <svg
+                                    v-else
+                                    xmlns="http://www.w3.org/2000/svg" 
+                                    fill="none" 
+                                    viewBox="0 0 24 24" 
+                                    stroke-width="1.5" 
+                                    stroke="currentColor" 
+                                    class="size-6 text-white">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                                </svg>
+                        </div>
+                    </div>
+                    <div
+                        v-if="dropdownMenu"
+                        class="absolute -bottom-68 -left-75 w-85 bg-white dark:bg-slate-600 rounded-lg shadow-2xl border border-gray-300 dark:border-slate-500">
+                            <div class="flex justify-start items-center gap-x-2 px-4 py-3">
+                                <img 
+                                    src="../../assets/img/300-27.jpg" 
+                                    alt="Profile image"
+                                    class="rounded-full w-13 h-13 p-1 border-2 dark:border-white">
+                                <p class="text-gray-800 dark:text-slate-300">
+                                    Carlos I.
+                                    <span class="block font-light text-sm dark:text-slate-400"> admin </span>
+                                </p>
+                            </div>
+                            <div class="border-b dark:border-slate-500"></div>
+                            <div class="mt-3">
+                                <div 
+                                    class=" hover:bg-gray-200 dark:hover:bg-slate-500 px-4 py-2 cursor-pointer"> 
+                                        <RouterLink
+                                            :to="{ name: 'admin-home' }"
+                                            class="flex justify-start items-center gap-x-2 uppercase text-gray-800 dark:text-slate-200 font-extralight">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+                                            </svg> Administrar
+                                        </RouterLink>
+                                </div>
+                                <div class=" hover:bg-gray-200 dark:hover:bg-slate-500 px-4 py-2 cursor-pointer"> 
+                                    <p class="flex justify-start items-center gap-x-2 uppercase text-gray-800 dark:text-slate-200 font-extralight">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                                        </svg> Perfil
+                                    </p>
+                                </div>
+                                <div class=" hover:bg-gray-200 dark:hover:bg-slate-500 px-4 py-2 cursor-pointer"> 
+                                    <p class="flex justify-start items-center gap-x-2 uppercase text-gray-800 dark:text-slate-200 font-extralight">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg> Configuración
+                                    </p>
+                                </div>
+                                <div class=" hover:bg-gray-200 dark:hover:bg-slate-500 px-4 py-2 cursor-pointer">
+                                    <button
+                                        @click="user.logOut "
+                                        class="uppercase font-light inline-flex items-center gap-1 text-red-500 cursor-pointer hover:opacity-75">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
+                                            </svg> Cerrar sesión
+                                    </button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
